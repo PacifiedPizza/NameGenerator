ca6c98ce32b2dc5e559f1edc6ee7d14d4a0c4ff8
PacifiedPizza <143260560+PacifiedPizza@users.noreply.github.com>
2024-01-10 20:04:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import generatesillyname from 'sillyname';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static("./public"));
 
